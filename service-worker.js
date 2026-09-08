b6604d85e9bd8bf026fb357657f6cc39e4f77798
JoanAida <valenzuelamoreno27@gmail.com>
2026-09-08 18:24:41 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,21 @@
-self.addEventListener('install',e=>self.skipWaiting())
+const CACHE_NAME = 'inventario-v1';
+const ASSETS = [
+  '/Inventario/',
+  '/Inventario/index.html',
+  '/Inventario/styles.css',
+  '/Inventario/app.js',
+  '/Inventario/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
